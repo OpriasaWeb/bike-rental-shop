4b90a70f555688b9d6a68e131f8495c4cbdb6c41
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-01-16 22:46:55 +0800

    Bike shop codes using BASH scripting with postgresql database

--- bike-shop.sh
+++ bike-shop.sh
@@ -85,12 +85,11 @@ RENT_MENU() {
         SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available = false WHERE bike_id = $BIKE_ID_TO_RENT")
 
         # get bike info
-        BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT ")
+        BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT")
         BIKE_INFO_FORMATTED=$(echo $BIKE_INFO | sed 's/ |/"/')
-
+        
         # send to main menu
-        MAIN_MENU "I have put you down for the $BIKE_INFO_FORMATTED, $(echo $CUSTOMER_NAME | sed -E 's/^ *| *$//g')"
-
+        MAIN_MENU "I have put you down for the $BIKE_INFO_FORMATTED Bike, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
       fi
     fi
   fi
@@ -100,19 +99,61 @@ RETURN_MENU() {
   # get customer info
   echo -e "\nWhat's your phone number?"
   read PHONE_NUMBER
+
   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
+
   # if not found
-  if [[ -z $CUSTOMER_ID ]]
+  if [[ -z $CUSTOMER_ID  ]]
   then
     # send to main menu
     MAIN_MENU "I could not find a record for that phone number."
   else
-    # get customer's rentals
-    # if no rentals
-    # send to main menu
 
+    # get customer's rentals
+    CUSTOMER_RENTALS=$($PSQL "SELECT bike_id, type, size FROM bikes INNER JOIN rentals USING(bike_id) INNER JOIN customers USING(customer_id) WHERE phone = '$PHONE_NUMBER' AND date_returned IS NULL ORDER BY bike_id ")
+    
+
+    # if no rentals
+    if [[ -z $CUSTOMER_RENTALS ]]
+    then
+      # send to main menu
+      MAIN_MENU "You do not have any bikes rented."
+    else
+      # display rented bikes
+      echo -e "\nHere are your rentals:"
+      echo "$CUSTOMER_RENTALS" | while read BIKE_ID BAR TYPE BAR SIZE
+      do
+        echo "$BIKE_ID) $SIZE\" $TYPE Bike"
+      done
+      # ask for bike to return
+      echo -e "\nWhich one would you like to return?"
+      read BIKE_ID_TO_RETURN
+      # if not a number
+      if [[ ! $BIKE_ID_TO_RETURN =~ ^[0-9]+$ ]]
+      then
+        # send to main menu
+        MAIN_MENU "That is not a valid bike number."
+      else
+        # check if input is rented
+        RENTAL_ID=$($PSQL "SELECT rental_id FROM rentals INNER JOIN customers USING(customer_id) WHERE phone = '$PHONE_NUMBER' AND bike_id = $BIKE_ID_TO_RETURN AND date_returned IS NULL ")
+        # if input not rented
+        if [[ -z $RENTAL_ID ]]
+        then
+          # send to main menu
+          MAIN_MENU "You do not have that bike rented."
+        else
+          # update date_returned
+          RETURN_BIKE_RESULT=$($PSQL "UPDATE rentals SET date_returned = NOW() WHERE rental_id = $RENTAL_ID ")
+          # set bike availability to true
+          SET_TO_TRUE_RESULT=$($PSQL "UPDATE bikes SET available = true WHERE bike_id = $BIKE_ID_TO_RETURN ")
+          # send to main menu
+          MAIN_MENU "Thank you for returning your bike."
+        fi
+        
+      fi
+      
+    fi
   fi
-  
 }
 
 EXIT() {
